afc95ec2fd1a06069ab3567fbf7b9a48952855bb
osozero <sametozer13@gmail.com>
2018-12-20 13:27:04 -0800

    TGAImage definitions done.

--- src/TGAImage.cpp
+++ src/TGAImage.cpp
@@ -1,12 +1,45 @@
 #include "TGAImage.h"
+#include <algorithm>
 
 
+bool TGAImage::loadRLEData(std::ifstream& in)
+{
+}
+
+bool TGAImage::unloadRLEData(std::ofstream& out)
+{
+}
+
+bool TGAImage::readTGAFile(const char* filename)
+{
+
+}
+
+bool TGAImage::writeTGAFile(const char* filename)
+{
+}
 
 TGAImage::TGAImage()
+	:width(0),height(0),bytesPerPixel(0),data(nullptr)
+{}
+
+TGAImage::TGAImage(int w, int h, int bpp)
+	:width(w),height(h),bytesPerPixel(bpp)
 {
+	auto size = width * height * bytesPerPixel;
+	data = new unsigned char[size]{};
+}
+
+TGAImage::TGAImage(const TGAImage& t)
+	:width(t.width),height(t.height),bytesPerPixel(t.bytesPerPixel)
+{
+	auto size = width * height * bytesPerPixel;
+	data = new unsigned char[size];
+	std::copy(t.data, t.data + size, data);
 }
 
 
 TGAImage::~TGAImage()
 {
+	delete[] data;
 }
--- src/TGAImage.h
+++ src/TGAImage.h
@@ -1,8 +1,52 @@
 #pragma once
+
+
+#pragma pack(push,1)
+#include <fstream>
+
+struct TGAHeader
+{
+	char idLength;
+	char colorMapType;
+	char dataTypeCode;
+	short colorMapOrigin;
+	short colorMapLength;
+	char colorMapDepth;
+	short xOrigin;
+	short yOrigin;
+	short width;
+	short height;
+	char bitsPerPixel;
+	char imageDescriptor;
+};
+#pragma pack(pop)
+
 class TGAImage
 {
+protected:
+	unsigned char *data;
+	int width;
+	int height;
+	int bytesPerPixel;
+
+	bool loadRLEData(std::ifstream &in);
+	bool unloadRLEData(std::ofstream &out);
+
 public:
+	enum Format
+	{
+		GRAYSCALE=1,
+		RGB=3,
+		RGBA=4
+	};
+
 	TGAImage();
+	TGAImage(int w, int h, int bpp);
+	TGAImage(const TGAImage& t);
+
+	bool readTGAFile(const char *filename);
+	bool writeTGAFile(const char *filename);
+
 	~TGAImage();
 };
 
