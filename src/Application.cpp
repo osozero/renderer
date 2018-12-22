5dc5c1082307d80ccad168e5b0cbbc749782f7ed
osozero <sametozer13@gmail.com>
2018-12-21 18:53:48 -0800

    triangle function implemented.

--- src/Application.cpp
+++ src/Application.cpp
@@ -12,32 +12,32 @@ const int height = 800;
 
 
 
-void line(int x0,int y0,int x1,int y1,TGAImage &image,const TGAColor &color)
+void line(Vec2i v1, Vec2i v2,TGAImage &image,const TGAColor &color)
 {
 	bool steep = false;
 
-	if(std::abs(x0-x1)<std::abs(y0-y1))
+	if(std::abs(v1.x-v2.x)<std::abs(v1.y-v2.y))
 	{
-		std::swap(x0, y0);
-		std::swap(x1, y1);
+		std::swap(v1.x, v1.y);
+		std::swap(v2.x, v2.y);
 		steep = true;
 	}
 
-	if(x0>x1)
+	if(v1.x>v2.x)
 	{
-		std::swap(x0, x1);
-		std::swap(y0, y1);
+		std::swap(v1.x, v2.x);
+		std::swap(v1.y, v2.y);
 	}
 
-	int dx = x1 - x0;
-	int dy = y1 - y0;
+	int dx = v2.x - v1.x;
+	int dy = v2.y - v1.y;
 
 	int derror = std::abs(dy) * 2;
 	int error = 0;
 
-	int y = y0;
+	int y = v1.y;
 
-	for(int x=x0;x<=x1;x++)
+	for(int x=v1.x;x<=v2.x;x++)
 	{
 		if(steep)
 		{
@@ -49,34 +49,34 @@ void line(int x0,int y0,int x1,int y1,TGAImage &image,const TGAColor &color)
 		error += derror;
 		if(error>dx)
 		{
-			y += (y1 > y0 ? 1 : -1);
+			y += (v2.y > v1.y? 1 : -1);
 			error -= dx*2;
 		}
 	}
 }
 
-
-int main(int argc,char**argv)
+void triangle(Vec2i v1,Vec2i v2,Vec2i v3,TGAImage &image, const TGAColor &color)
 {
-	std::string arg;
-	
-	if(argc==2)
-	{
-		arg = argv[1];
-	}else
-	{
-		arg = "resource/model/head.obj";
-	}
+	line(v1, v2, image, color);
+	line(v2, v3, image, color);
+	line(v3, v1, image, color);
+}
+
+
+
+void loadModelAndDrawWireframe()
+{
+	std::string arg = "resource/model/head.obj";
 
 	auto model = std::make_unique<Model>(Model(arg.c_str()));
 
 	TGAImage image(width, height, TGAImage::RGB);
 
-	for(int i=0;i<model->numberOfFaces();i++)
+	for (int i = 0; i < model->numberOfFaces(); i++)
 	{
 		std::vector<int>  face = model->face(i);
 
-		for(int j=0;j<3;j++)
+		for (int j = 0; j < 3; j++)
 		{
 			auto v0 = model->vert(face[j]);
 			auto v1 = model->vert(face[(j + 1) % 3]);
@@ -87,11 +87,21 @@ int main(int argc,char**argv)
 			int x1 = (v1.x + 1.0)*width / 2.0;
 			int y1 = (v1.y + 1.0)*height / 2.0;
 
-			line(x0, y0, x1, y1, image, white);
+			line(Vec2i(x0, y0), Vec2i(x1, y1), image, white);
 		}
 	}
 
 	image.flipVertically();
 	image.writeTGAFile("out.tga");
+}
+int main(int argc,char**argv)
+{
+	TGAImage image(width, height, TGAImage::RGB);
+
+	triangle(Vec2i(10, 90), Vec2i(80, 90), Vec2i(200, 250), image, white);
+
+	image.flipVertically();
+	image.writeTGAFile("out2.tga");
+	
 	return 0;
 }
