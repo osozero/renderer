957e62a061c9c7f453ddd704f1861a68a286c75f
osozero <sametozer13@gmail.com>
2019-01-04 19:03:27 -0800

    shadow shadig implemented.

---
+++ src/ShadowShader.cpp
@@ -0,0 +1,68 @@
+#include "ShadowShader.h"
+#include <glm/gtc/matrix_access.inl>
+#include <algorithm>
+
+
+Vec4f ShadowShader::vertex(int iface, int nthVert)
+{
+	varying_uv[nthVert] = model.texture(iface, nthVert);
+	Vec4f glVertex = Vec4f(model.vert(iface, nthVert),1.0f);
+
+	glm::vec4 glmV4glVertex(glVertex.x, glVertex.y, glVertex.z, glVertex.w);
+
+	glmV4glVertex = glmV4glVertex * modelview*proj*viewport;
+
+	varying_tri = glm::column(varying_tri, nthVert, glmV4glVertex / glmV4glVertex[3]);
+
+	return Vec4f(glmV4glVertex.x, glmV4glVertex.y, glmV4glVertex.z, glmV4glVertex.w);
+}
+
+bool ShadowShader::fragment(Vec3f bar, TGAColor& color)
+{
+	int width = 800;
+	glm::vec3 glmBar(bar.x, bar.y, bar.z);
+
+	glm::vec4 shadowbufferPoint = glm::vec4(glmBar * varying_tri,1.0f)*uniform_MShadow;
+
+	shadowbufferPoint = shadowbufferPoint / shadowbufferPoint[3];
+
+	int index = int(shadowbufferPoint[0]) + shadowbufferPoint[1] * width;
+
+	float shadow = 0.3 + 7 * (shadowbuffer[index] < shadowbufferPoint[2]);
+
+	Vec3f interpolatedTexCoords(0, 0, 0);
+	float temp;
+	for (int i = 0; i < 3; i++)
+	{
+		temp = 0;
+		for (int j = 0; j < 3; j++)
+		{
+			temp += varying_uv[j][i] * bar[j];
+		}
+
+		interpolatedTexCoords[i] = temp;
+	}
+
+	Vec3f normal = normalTexture.getNormalFromTexCoord(interpolatedTexCoords);
+
+	glm::vec3 n = glm::normalize(glm::vec4(normal.x, normal.y, normal.z,1.0f) * uniform_MT);
+
+	glm::vec3 l = glm::normalize(lightDir*uniform_M);
+
+	glm::vec3 r = glm::normalize(n*(n*l*2.0f) - l);
+
+	float spec = pow(std::max(r.z, 0.0f), specularTexture.getSpecularFromTexCoords(interpolatedTexCoords));
+
+	float diff = std::max(0.0f, glm::dot(n, l));
+
+	TGAColor c = diffuseTexture.getColorFromTexCoord(interpolatedTexCoords);
+
+	for(int i=0;i<3;i++)
+	{
+		color[i] = std::min<float>(20 + c[i] * shadow*(1.2*diff + 0.6*spec), 255);
+	}
+
+	return false;
+
+
+}
---
+++ src/ShadowShader.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "IShader.h"
+#include <glm/mat4x2.hpp>
+#include "Model.h"
+#include "Texture.h"
+
+class ShadowShader :
+	public IShader
+{
+public:
+
+	glm::mat4 uniform_M;//projection*modelview;
+	glm::mat4 uniform_MT;//uniform_M.inverse.transpose
+	glm::mat4 uniform_MShadow;
+	
+	Vec3f varying_uv[3];//texture color mapping
+	glm::mat3x3 varying_tri;
+
+	Model model;
+	Texture diffuseTexture;
+	Texture normalTexture;
+	Texture specularTexture;
+
+	float *shadowbuffer;
+
+	glm::vec4 lightDir;
+
+	glm::mat4 viewport;
+	glm::mat4 proj;
+	glm::mat4 modelview;
+
+
+	ShadowShader(Model &_model, Texture &_dTexture, Texture &_nTexture, Texture &_sTexture, glm::mat4 &_viewport, glm::mat4 &_proj, glm::mat4 &_modelview, glm::mat4 M, glm::mat4 MIT, glm::mat4 MS,float * shadownuffer_,glm::vec4 lightDir_)
+		:model(_model),
+		diffuseTexture(_dTexture),
+		normalTexture(_nTexture),
+		specularTexture(_sTexture),
+		viewport(_viewport),
+		proj(_proj),
+		modelview(_modelview),
+		uniform_M(M),
+		uniform_MT(MIT),
+		uniform_MShadow(MS),
+		shadowbuffer(shadownuffer_),
+		lightDir(lightDir_)
+	{};
+
+	~ShadowShader(){};
+	Vec4f vertex(int iface, int nthVert) override;
+	bool fragment(Vec3f bar, TGAColor& color) override;
+};
+
